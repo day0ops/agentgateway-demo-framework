b6173ffa69f1e4f6136eeb599a9ac3c84525583e
Kasun Talwatta <kasun.talwatta@gmail.com>
2026-05-14 13:10:53 +1200

    feat: add addon workshop.js sidecar files

---
+++ addons/solo-ui/workshop.js
@@ -0,0 +1,44 @@
+// addons/solo-ui/workshop.js
+
+export function envVarsFor(_cfg) {
+  return [];
+}
+
+export function envExportsFor(_cfg) {
+  return [
+    { key: 'SOLO_UI_VERSION', value: '0.3.13', group: 'versions' },
+    { key: 'SOLO_UI_NAMESPACE', value: 'agentgateway-system', group: 'settings' },
+  ];
+}
+
+export async function generate(_subIndex, _cfg) {
+  const lines = [];
+  lines.push('### Install Solo UI');
+  lines.push('');
+  lines.push('Installs the Solo UI management console (includes ClickHouse).');
+  lines.push('');
+  lines.push('#### Install Solo UI CRDs');
+  lines.push('');
+  lines.push('```bash');
+  lines.push('helm upgrade -i solo-ui-crds \\');
+  lines.push(
+    '  oci://us-docker.pkg.dev/solo-public/solo-enterprise-helm/charts/management-crds \\',
+  );
+  lines.push('  -n ${SOLO_UI_NAMESPACE} \\');
+  lines.push('  --version ${SOLO_UI_VERSION} \\');
+  lines.push('  --create-namespace \\');
+  lines.push('  --wait --timeout 5m');
+  lines.push('```');
+  lines.push('');
+  lines.push('#### Install Solo UI');
+  lines.push('');
+  lines.push('```bash');
+  lines.push('helm upgrade -i solo-ui \\');
+  lines.push('  oci://us-docker.pkg.dev/solo-public/solo-enterprise-helm/charts/management \\');
+  lines.push('  -n ${SOLO_UI_NAMESPACE} \\');
+  lines.push('  --version ${SOLO_UI_VERSION} \\');
+  lines.push('  --create-namespace \\');
+  lines.push('  --wait --timeout 5m');
+  lines.push('```');
+  return lines.join('\n');
+}
---
+++ addons/telemetry/workshop.js
@@ -0,0 +1,73 @@
+// addons/telemetry/workshop.js
+
+export function envVarsFor(_cfg) {
+  return [];
+}
+
+export function envExportsFor(_cfg) {
+  return [
+    { key: 'PROMETHEUS_STACK_VERSION', value: '80.4.2', group: 'versions' },
+    { key: 'TEMPO_VERSION', value: '1.29.0', group: 'versions' },
+    { key: 'LOKI_VERSION', value: '6.6.2', group: 'versions' },
+    { key: 'ALLOY_VERSION', value: '0.12.0', group: 'versions' },
+    { key: 'TELEMETRY_NAMESPACE', value: 'telemetry', group: 'settings' },
+  ];
+}
+
+export async function generate(_subIndex, _cfg) {
+  const lines = [];
+  lines.push('### Install Telemetry Stack');
+  lines.push('');
+  lines.push(
+    'Installs Prometheus/Grafana, Tempo Distributed, Loki, and Alloy for full observability.',
+  );
+  lines.push('');
+  lines.push('#### Add Helm repositories');
+  lines.push('');
+  lines.push('```bash');
+  lines.push('helm repo add prometheus-community https://prometheus-community.github.io/helm-charts');
+  lines.push('helm repo add grafana https://grafana.github.io/helm-charts');
+  lines.push('helm repo update');
+  lines.push('```');
+  lines.push('');
+  lines.push('#### Install kube-prometheus-stack');
+  lines.push('');
+  lines.push('```bash');
+  lines.push('helm upgrade -i kube-prometheus-stack prometheus-community/kube-prometheus-stack \\');
+  lines.push('  -n ${TELEMETRY_NAMESPACE} \\');
+  lines.push('  --version ${PROMETHEUS_STACK_VERSION} \\');
+  lines.push('  --create-namespace \\');
+  lines.push('  --wait');
+  lines.push('```');
+  lines.push('');
+  lines.push('#### Install tempo-distributed');
+  lines.push('');
+  lines.push('```bash');
+  lines.push('helm upgrade -i tempo grafana/tempo-distributed \\');
+  lines.push('  -n ${TELEMETRY_NAMESPACE} \\');
+  lines.push('  --version ${TEMPO_VERSION} \\');
+  lines.push('  --create-namespace \\');
+  lines.push('  --wait');
+  lines.push('```');
+  lines.push('');
+  lines.push('#### Install loki');
+  lines.push('');
+  lines.push('```bash');
+  lines.push('helm upgrade -i loki grafana/loki \\');
+  lines.push('  -n ${TELEMETRY_NAMESPACE} \\');
+  lines.push('  --version ${LOKI_VERSION} \\');
+  lines.push('  --create-namespace \\');
+  lines.push('  --wait');
+  lines.push('```');
+  lines.push('');
+  lines.push('#### Install alloy');
+  lines.push('');
+  lines.push('```bash');
+  lines.push('helm upgrade -i alloy grafana/alloy \\');
+  lines.push('  -n ${TELEMETRY_NAMESPACE} \\');
+  lines.push('  --version ${ALLOY_VERSION} \\');
+  lines.push('  --create-namespace \\');
+  lines.push('  --wait');
+  lines.push('```');
+  return lines.join('\n');
+}
